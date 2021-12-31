7e5b266931cc28bb38a00153a45c088f2ded901b
AL EMRAN <emrancu1@gmail.com>
2021-12-31 15:31:53 +0600

    update vite config

--- vite/VueBuilder.js
+++ vite/VueBuilder.js
@@ -1,19 +1,18 @@
-
 let path = require('path');
 
-const basePath =  __dirname +  path.sep +'..'+path.sep +'..'+path.sep +'..'+ path.sep  ;
+const basePath = __dirname + path.sep + '..' + path.sep + '..' + path.sep + '..' + path.sep;
 
 const config = {
-    plugins:  [],
+    plugins: [],
     build: {
         emptyOutDir: true,
-        manifest: true,
+        manifest: true, 
         rollupOptions: {
             input: {},
             output: {
-                entryFileNames: `assets/[name].js`,
-                chunkFileNames: `assets/[name].js`,
-                assetFileNames: `assets/[name].[ext]`
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`
             }
         },
     },
@@ -22,7 +21,14 @@ const config = {
         fs: {
             strict: false,
         },
+        hmr: {
+            protocol: 'ws',
+            host: 'localhost'
+        },
         port: 3000
+    },
+    resolve: {
+        alias: {}
     }
 }
 
@@ -36,25 +42,28 @@ class PluginMasterVueConfig {
 
     getConfig(customConfig) {
 
-        let rollupInput =  {}
+        let rollupInput = {}
 
-        customConfig.input.forEach(item => { 
+        customConfig.input.forEach(item => {
             let fileName = item.outputFileName.split(".")[0];
-            rollupInput[fileName] =   item.source
+            rollupInput[fileName] = item.source
         });
 
         this.viteConfig.plugins = customConfig.plugins
-        this.viteConfig.build.rollupOptions.input  = rollupInput
-         this.viteConfig.build.outDir  =   path.join( this.basePath, customConfig.outputDir)
-
-        if(customConfig.port){
-           this.viteConfig.server.port = customConfig.port
+        this.viteConfig.build.rollupOptions.input = rollupInput
+        this.viteConfig.build.outDir = path.join(this.basePath, customConfig.outputDir)
+        if (customConfig.alias) {
+            this.viteConfig.resolve.alias = customConfig.alias
         }
 
-        return  this.viteConfig ;
+        if (customConfig.port) {
+            this.viteConfig.server.port = customConfig.port
+        }
+
+        return this.viteConfig;
     }
 
 }
 
 
-module.exports =  new PluginMasterVueConfig(config, basePath)
+module.exports = new PluginMasterVueConfig(config, basePath)
